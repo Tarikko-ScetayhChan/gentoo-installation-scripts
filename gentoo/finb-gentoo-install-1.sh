f961ca84e5cb43b04a020576d299b179ad8e0b0d
Tarikko-ScetayhChan <weychon_lyon@outlook.com>
2024-04-21 14:03:08 +0800

    Update files

--- gentoo/finb-gentoo-install-1.sh
+++ gentoo/finb-gentoo-install-1.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
 
-# to locate finb path
+set -e
+
+# locate finb path
 echo
 FINB_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 echo -e "[\e[32mInfo\e[0m] Located the finb path (FINB_PATH=${FINB_PATH})."
 
-# to get current script name
+# get current script name
 echo
-FINB_CURRENT_SCRIPT=`basename "$0"`
-echo -e "[\e[32mInfo\e[0m] Gotten the current script name (FINB_CURRENT_SCRIPT=${FINB_CURRENT_SCRIPT})."
+FINB_GENTOO_CURRENT_SCRIPT=`basename "$0"`
+echo -e "[\e[32mInfo\e[0m] Gotten the current script name (FINB_GENTOO_CURRENT_SCRIPT=${FINB_GENTOO_CURRENT_SCRIPT})."
 
-# to load finb core
+# load finb core
 echo
 source ${FINB_PATH}/core/finb.loadcore
 
-# to load script configuration
+# load script instances
+echo
+source ${0%/*}/instance/*
+finb.msg info "Loaded the script instances."
+
+# load script configuration
 echo
 source ${0%/*}/conf/*
+FINB_GENTOO_TOTAL_STEP=114514
+FINB_GENTOO_CURRENT_STEP=0
 finb.msg info "Loaded the script configuration."
 
-# to check variables
+# check variables
 
 # print scripthead
-finb.scripthead ${FINB_CURRENT_SCRIPT}
+finb.scripthead ${FINB_GENTOO_CURRENT_SCRIPT}
 sleep 3
+
+# set and print total number of steps
+finb.msg.settotalstep ${FINB_GENTOO_TOTAL_STEP}
+sleep 3
+echo
 
 # remind to edit configuration
 finb.msg.conf
@@ -32,5 +46,36 @@ echo
 
 # wait 5 seconds for regret
 finb.msg.regret
-echo
+
+#################################################################
+
+((FINB_GENTOO_CURRENT_STEP=${FINB_GENTOO_CURRENT_STEP}+1))
+finb.stepon ${FINB_GENTOO_CURRENT_STEP} ${FINB_GENTOO_TOTAL_STEP} net "To configure the network"
+
+if [ ${FINB_GENTOO_NETWORK_CONNECTION_METHOD} == "net-setup" ]
+then
+    net-setup
+    sleep 1
+elif [ ${FINB_GENTOO_NETWORK_CONNECTION_METHOD} == "wpa_supplicant" ]
+then
+    wpa_supplicant -D wext -B -i ${FINB_GENTOO_NETWORK_CONNECTION_WPA_SUPPLICANT_DEVICE} -c <(wpa_passphrase ${FINB_GENTOO_NETWORK_CONNECTION_WPA_SUPPLICANT_WLANNAME} ${FINB_GENTOO_NETWORK_CONNECTION_WPA_SUPPLICANT_PASSWORD})
+    sleep 1
+elif [ ${FINB_GENTOO_NETWORK_CONNECTION_METHOD} == "custom" ]
+then
+    bash ${FINB_GENTOO_NETWORK_CONNECTION_CUSTOM_COMMAND}
+    sleep 1
+elif [ ${FINB_GENTOO_NETWORK_CONNECTION_METHOD} == "skip" ]
+then
+    finb.msg info "Skipped network connection."
+    sleep 1
+fi
+
+ping -c ${FINB_GENTOO_NETWORK_CONNECTION_TEST_TIMES} ${FINB_GENTOO_NETWORK_CONNECTION_TEST_DOMAIN}
+
+#################################################################
+
+((FINB_GENTOO_CURRENT_STEP=${FINB_GENTOO_CURRENT_STEP}+1))
+finb.stepon ${FINB_GENTOO_CURRENT_STEP} ${FINB_GENTOO_TOTAL_STEP} dns "To configure the DNS"
+
+finb.gentoo.chdns ${FINB_GENTOO_DNS}
 
