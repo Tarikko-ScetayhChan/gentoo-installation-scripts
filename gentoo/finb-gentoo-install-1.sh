a81d1e4fb291a092274f89c946a4e8fad2cc058d
Tarikko-ScetayhChan <weychon_lyon@outlook.com>
2024-04-21 21:55:07 +0800

    Update files

--- gentoo/finb-gentoo-install-1.sh
+++ gentoo/finb-gentoo-install-1.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-set -e
-
 # locate finb path
 echo
 FINB_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-echo -e "[\e[32mInfo\e[0m] Located the finb path (FINB_PATH=${FINB_PATH})."
+echo -e "[\e[032mInfo\e[0m] Located the finb path (FINB_PATH=${FINB_PATH})."
 
 # get current script name
 echo
 FINB_GENTOO_CURRENT_SCRIPT=`basename "$0"`
-echo -e "[\e[32mInfo\e[0m] Gotten the current script name (FINB_GENTOO_CURRENT_SCRIPT=${FINB_GENTOO_CURRENT_SCRIPT})."
+echo -e "[\e[032mInfo\e[0m] Gotten the current script name (FINB_GENTOO_CURRENT_SCRIPT=${FINB_GENTOO_CURRENT_SCRIPT})."
 
 # load finb core
 echo
+chmod +x ${FINB_PATH}/core/*
 source ${FINB_PATH}/core/finb.loadcore
-
-# load script instances
-echo
-source ${0%/*}/instance/*
-finb.msg info "Loaded the script instances."
 
 # load script configuration
 echo
@@ -50,32 +44,58 @@ finb.msg.regret
 #################################################################
 
 ((FINB_GENTOO_CURRENT_STEP=${FINB_GENTOO_CURRENT_STEP}+1))
-finb.stepon ${FINB_GENTOO_CURRENT_STEP} ${FINB_GENTOO_TOTAL_STEP} net "To configure the network"
+finb.step on ${FINB_GENTOO_CURRENT_STEP} ${FINB_GENTOO_TOTAL_STEP} net "To configure the network"
 
-if [ ${FINB_GENTOO_NETWORK_CONNECTION_METHOD} == "net-setup" ]
-then
-    net-setup
-    sleep 1
-elif [ ${FINB_GENTOO_NETWORK_CONNECTION_METHOD} == "wpa_supplicant" ]
-then
-    wpa_supplicant -D wext -B -i ${FINB_GENTOO_NETWORK_CONNECTION_WPA_SUPPLICANT_DEVICE} -c <(wpa_passphrase ${FINB_GENTOO_NETWORK_CONNECTION_WPA_SUPPLICANT_WLANNAME} ${FINB_GENTOO_NETWORK_CONNECTION_WPA_SUPPLICANT_PASSWORD})
-    sleep 1
-elif [ ${FINB_GENTOO_NETWORK_CONNECTION_METHOD} == "custom" ]
-then
-    bash ${FINB_GENTOO_NETWORK_CONNECTION_CUSTOM_COMMAND}
-    sleep 1
-elif [ ${FINB_GENTOO_NETWORK_CONNECTION_METHOD} == "skip" ]
-then
-    finb.msg info "Skipped network connection."
-    sleep 1
-fi
+until 
+    if [[ ${FINB_GENTOO_NETWORK_CONNECTION_METHOD} == "net-setup" ]] 
+    then
+        net-setup
+        sleep 1
+    elif [[ ${FINB_GENTOO_NETWORK_CONNECTION_METHOD} == "wpa_supplicant" ]] 
+    then
+       wpa_supplicant -D wext -B -i ${FINB_GENTOO_NETWORK_CONNECTION_WPA_SUPPLICANT_DEVICE} -c <(wpa_passphrase ${FINB_GENTOO_NETWORK_CONNECTION_WPA_SUPPLICANT_WLANNAME} ${FINB_GENTOO_NETWORK_CONNECTION_WPA_SUPPLICANT_PASSWORD})
+       sleep 1
+    elif [[ ${FINB_GENTOO_NETWORK_CONNECTION_METHOD} == "custom" ]] 
+    then
+      bash ${FINB_GENTOO_NETWORK_CONNECTION_CUSTOM_COMMAND}
+      sleep 1
+    elif [[ ${FINB_GENTOO_NETWORK_CONNECTION_METHOD} == "skip" ]] 
+    then
+       finb.msg info "Skipped network connection."
+       sleep 1
+       echo
+    fi
+    ping -c ${FINB_GENTOO_NETWORK_CONNECTION_TEST_TIMES} ${FINB_GENTOO_NETWORK_CONNECTION_TEST_DOMAIN}
+do
+    finb.msg info "Network connected."
+    echo
+done
 
-ping -c ${FINB_GENTOO_NETWORK_CONNECTION_TEST_TIMES} ${FINB_GENTOO_NETWORK_CONNECTION_TEST_DOMAIN}
+finb.step off ${FINB_GENTOO_CURRENT_STEP} ${FINB_GENTOO_TOTAL_STEP} net "To configure the network"
 
 #################################################################
 
 ((FINB_GENTOO_CURRENT_STEP=${FINB_GENTOO_CURRENT_STEP}+1))
-finb.stepon ${FINB_GENTOO_CURRENT_STEP} ${FINB_GENTOO_TOTAL_STEP} dns "To configure the DNS"
+finb.step on ${FINB_GENTOO_CURRENT_STEP} ${FINB_GENTOO_TOTAL_STEP} dns "To configure the DNS"
 
 finb.gentoo.chdns ${FINB_GENTOO_DNS}
 
+finb.step off ${FINB_GENTOO_CURRENT_STEP} ${FINB_GENTOO_TOTAL_STEP} dns "To configure the DNS"
+
+#################################################################
+
+((FINB_GENTOO_CURRENT_STEP=${FINB_GENTOO_CURRENT_STEP}+1))
+finb.step on ${FINB_GENTOO_CURRENT_STEP} ${FINB_GENTOO_TOTAL_STEP} disk "To partition the disk, mkfs and mount"
+
+if [[ ${FINB_GENTOO_DISK_PARTITON_AND_MKFS_METHOD} == "auto" ]]
+then
+    echo -e "g\nn\n1\n\n+1G\nt\n1\nn\n2\n\n+1G\nn\n3\n\n+4G\nn\n4\n\n\nw" | fdisk ${FINB_GENTOO_DISK_PARTITON_AND_MKFS_DISK}
+    mkfs.fat -F 32 ${FINB_GENTOO_DISK_PARTITON_AND_MKFS_DISK}p1
+    mkfs.ext4 ${FINB_GENTOO_DISK_PARTITON_AND_MKFS_DISK}p2
+    mkswap ${FINB_GENTOO_DISK_PARTITON_AND_MKFS_DISK}p3
+    mkfs.xfs ${FINB_GENTOO_DISK_PARTITON_AND_MKFS_DISK}p4
+    mount -v ${path_root_parition} --mkdir /mnt/gentoo
+    mount -v ${path_boot_parition} --mkdir /mnt/gentoo/boot
+    mount -v ${path_efi_parition} --mkdir /mnt/gentoo/boot/efi
+    swapon -v ${path_swap_parition}
+fi
