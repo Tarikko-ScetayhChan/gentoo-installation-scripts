d836e799014c1f013fee1b264e6a2e7322690e79
Tarikko-ScetayhChan <weychon_lyon@outlook.com>
2024-07-05 14:44:11 +0800

    turned PrintStep to PrintStepOn/PrintStepOff

--- install-3.sh
+++ install-3.sh
@@ -10,6 +10,8 @@ cd
 swapoff /mnt/gentoo/swapfile
 umount -l /mnt/gentoo/dev{/shm,/pts,}
 umount -lR /mnt/gentoo
+PrintStepOff 1 2 "To unmount file systems"
 
 PrintStepOn 2 2 "To reboot the system"
 reboot
+PrintStepOff 2 2 "To reboot the system"
